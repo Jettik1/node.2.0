5554413174a61eebb85a4b6df8a1e111e20433be
Jettio <contrakt120@gmail.com>
2022-02-01 03:44:01 +0300

    s work end

--- lessons/os.js
+++ lessons/os.js
@@ -1,26 +1,27 @@
 const os = require('os');
 const cluster = require('cluster');
 
-// console.log(os.platform());
-// console.log(os.arch());
-// console.log(os.cpus().length);
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.cpus().length);
 
-// if (cluster.isMaster) {
-//     for (let i = 0; i < os.cpus().length - 2; i++) {
-//         cluster.fork()
-//     }
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`Воркер с pid = ${worker.process.pid} умер`)
-//         if(code ===) {
-//             cluster.fork()
-//         } else {
-//             console.log('Воркер умер...')
-//         }
-//     })
-// } else {
-//     console.log(`Воркер с pid= ${process.pid} запущен`)
-//
-//     setInterval(() => {
-//         console.log(`Воркер с pid= ${process.pid} еще работает`)
-//     }, 5000)
-// }
+if (cluster.isMaster) {
+    for (let i = 0; i < os.cpus().length - 2; i++) {
+        cluster.fork()
+    }
+    cluster.on('exit', (worker, code, signal) => { // Подписались НА событие exit 
+        console.log(`Воркер с pid = ${worker.process.pid} умер`)
+        // cluster.fork() // раскоментируй, если иф ниже закоментирован
+        /* if(code ===) { // запускает новый дочерний процесс при каком-то условии
+            cluster.fork() // вытащи из коментирования выше чтобы запускались новые дочерне процессы
+        } else {
+            console.log('Воркер умер...')
+        } */
+    })
+} else {
+    console.log(`Воркер с pid= ${process.pid} запущен`)
+
+    setInterval(() => {
+        console.log(`Воркер с pid= ${process.pid} еще работает`)
+    }, 5000)
+}
